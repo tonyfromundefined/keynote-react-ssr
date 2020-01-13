73d8bcb23271d7d88ecd9b0f77702917013619d1
Tony <jeehyukwon@gmail.com>
2020-01-14 00:49:08 +0900

    feat(react): init react-client

---
+++ servers/react-client/src/constants/session.ts
@@ -0,0 +1,3 @@
+export const SESSION_SECRET = 'Hello, Community Day!'
+export const REDIS_HOST = 'localhost'
+export const REDIS_PORT = 54359
---
+++ servers/react-client/src/index.ts
@@ -0,0 +1,31 @@
+import connectRedis from 'connect-redis'
+import express from 'express'
+import expressSession from 'express-session'
+import next from 'next'
+import redis from 'redis'
+import { IS_PROD } from './constants'
+import conf from './next.config'
+
+const PORT = IS_PROD ? 80 : 3001
+
+async function bootstrap() {
+  const app = express()
+
+  const _next = next({
+    conf,
+    dev: !IS_PROD,
+    dir: './src',
+  })
+  await _next.prepare()
+
+  const handle = _next.getRequestHandler()
+
+  app.get('/health', (req, res) => res.json('ok'))
+  app.all('*', (req, res) => handle(req, res))
+
+  app.listen(PORT, () => {
+    console.log('React Renderer is running on http://localhost:' + PORT)
+  })
+}
+
+bootstrap()
